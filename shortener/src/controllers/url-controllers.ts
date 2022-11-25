b322c231702d6139e4acc348389c2fa8b18a2fd6
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-25 20:31:12 +0300

    set up base controllers

---
+++ shortener/src/controllers/url-controllers.ts
@@ -0,0 +1,10 @@
+import { validationResult } from 'express-validator';
+import { Url } from '../models/Url';
+
+const getUrls = async () => {};
+
+const createUrl = async () => {};
+
+const pingUrl = async () => {};
+
+export { getUrls, createUrl, pingUrl };
