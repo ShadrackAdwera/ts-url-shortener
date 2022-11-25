fddd7d8462c003f6202839eafda4aebc11c5f9ce
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-25 20:26:45 +0300

    create url model

--- shortener/src/models/Url.ts
+++ shortener/src/models/Url.ts
@@ -1,1 +1,24 @@
-//
+import { Document, Model, model, Schema } from 'mongoose';
+
+interface UrlDoc extends Document {
+  longUrl: string;
+  shortUrl: string;
+  version: number;
+}
+
+interface UrlModel extends Model<UrlDoc> {
+  longUrl: string;
+  shortUrl: string;
+}
+
+const urlSchema = new Schema(
+  {
+    longUrl: { type: String, required: true },
+    shortUrl: { type: String, required: true },
+  },
+  { timestamps: true, toJSON: { getters: true } }
+);
+
+const Url = model<UrlDoc, UrlModel>('url', urlSchema);
+
+export { Url };
