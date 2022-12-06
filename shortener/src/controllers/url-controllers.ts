ab5050685793917c64ca9b7595e2be647e434aa2
ShadrackAdwera <adweshshaddie@gmail.com>
2022-12-06 11:57:30 +0300

    add POST controller

--- shortener/src/controllers/url-controllers.ts
+++ shortener/src/controllers/url-controllers.ts
@@ -1,9 +1,51 @@
+import { HttpError } from '@adwesh/common';
+import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
-import { Url } from '../models/Url';
+import { Url, UrlDoc } from '../models/Url';
+import { sha } from '../utils/sha';
 
-const getUrls = async () => {};
+const getUrls = async (req: Request, res: Response, next: NextFunction) => {
+  let urls: (UrlDoc & { _id: string })[];
+  try {
+    urls = await Url.find();
+  } catch (error) {
+    return next(
+      new HttpError(
+        error instanceof Error ? error.message : 'An error occured',
+        500
+      )
+    );
+  }
+  res.status(200).json({ count: urls.length, urls });
+};
 
-const createUrl = async () => {};
+const createUrl = async (req: Request, res: Response, next: NextFunction) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) return next(new HttpError('Invalid inputs', 400));
+  const { url } = req.body;
+
+  const shortened = sha(url);
+
+  const newUrl = new Url({ longUrl: url, shortUrl: shortened });
+
+  try {
+    await newUrl.save();
+  } catch (error) {
+    return next(
+      new HttpError(
+        error instanceof Error ? error.message : 'An error occured',
+        500
+      )
+    );
+  }
+  res
+    .status(201)
+    .json({
+      message: 'URL Created',
+      shortUrl: newUrl.shortUrl,
+      longUrl: newUrl.longUrl,
+    });
+};
 
 const pingUrl = async () => {};
 
